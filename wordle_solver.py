62189e3339eddfaf4c0e6a90d4220a5c849897fb
Joseph Boen <josephboen@DNa1cbc04.SUNet>
2024-03-10 19:33:30 -0700

    added strategy simulator. Refactored code for speed.

--- wordle_solver.py
+++ wordle_solver.py
@@ -10,11 +10,30 @@ from utils import (load_words,
                    guess2color, 
                    get_color_distribution, 
                    get_expected_information,
-                   postprocessing_heuristic)
+                   build_guess_df, 
+                   next_guess_options,
+                   filter_answers)
 
-def execute_wordle_solver(board, valid_words, starting_guesses, verbose = True):
+def execute_wordle_solver(board: list, valid_words: list, starting_guesses: pd.DataFrame, 
+                                                                verbose: bool = True, 
+                                                                show_progress: bool = True,
+                                                                precomputed_outputs: dict = None):
     
-    #TODO supress prints for fast simulation testing
+    '''
+    Suggests guesses for wordle games depending on board. 
+    
+    Args
+        - board: list of tuples guess and inputs
+        - valid_words: list of wordle words
+        - starting_guesses: dataframe with columns guess and information for first guess
+        - verbose: print outputs or not
+        - show_progress: show tqdm progress bar
+        - precomputed_outputs: cached input output pairs
+        
+    Returns
+        - guess_df: dataframe with columns guess and information for next guess
+    '''
+    
     
     if len(board) == 0:
         
@@ -26,64 +45,76 @@ def execute_wordle_solver(board, valid_words, starting_guesses, verbose = True):
         
     else:
         possible_answers = valid_words
-        for (guess, output) in board:
-            
-            if output == "GGGGG":
-                print("Congratulations! The correct answer was {}".format(guess))
+        
+        if "GGGGG" in np.asarray(board).ravel() and verbose:
+            print("Congratulations! The correct answer was {}".format(board[-1][0]))
+            return None
+        
+        else:
+            try:
+                possible_answers = filter_answers(board, valid_words, 
+                                                  precomputed_outputs = precomputed_outputs)
+            except:
+                print("We're sorry. There don't seem to be any answers that fit those outputs.")
                 return None
             
-            else:      
-                color_distribution = get_color_distribution(guess, possible_answers)
-                possible_answers = color_distribution[output]
-            
-        guess_information = get_expected_information(guesses = valid_words, 
-                                                     answers = possible_answers,
-                                                     show_progress = True)
-        
-        lucky_guesses = postprocessing_heuristic(guess_information, possible_answers)
-        
-        display(possible_answers, guess_information, lucky_guesses)
-   
-        return guess_information, possible_answers, lucky_guesses
+            guess_information = get_expected_information(guesses = valid_words, 
+                                                         answers = possible_answers,
+                                                         show_progress = show_progress,
+                                                         precomputed_outputs = precomputed_outputs)
 
-def display(possible_answers, guess_information, lucky_guesses):
+            guess_df = build_guess_df(guess_information, possible_answers)
+
+            if verbose:
+                display(guess_df, ["best_info", "best_answer"])
+
+            return guess_df
+
+def display(guess_df, strategies_list, n=20):
     '''
     Nicely formats and prints outputs
     
     Args
-        - possible_answers: remaining words that satisfy previous outputs
-        - guess_information: dict with keys guess, vals information
-        - lucky_guesses: list of potentially lucky glesses
+        - guess_df: pandas dataframe with columns guess (str), 
+                    information (float), and possible answer (bool)
+        - strategies_list: list of best_answer, best_info, random_answer
     
     Returns
         - None. Simply prints nice looking messages
     '''
     
+    all_guesses_output = []
+    all_answers_output = []
+
+    for index, row in guess_df.iterrows():
+        row_str = "{} {:.3f}".format(row.guess, row.information)
+
+        if row.possible_answer:
+            all_answers_output.append(row_str)
+            all_guesses_output.append(row_str + " *")
+        else:
+            all_guesses_output.append(row_str)
+
     cli = cmd.Cmd()
     
-    next_guess = pd.DataFrame({"guess": list(guess_information.keys()),
-                                  "information": list(guess_information.values())})
+    print("There are {} possible answers remaining. \n".format(guess_df['possible_answer'].sum()))
     
-    print("There are {} possible answers remaining: \n".format(len(possible_answers)))
-        
-    cli.columnize(list(possible_answers)[:30], displaywidth=80)
-    if len(possible_answers) > 30: print("...")
+    print("All Guesses Ranked by Information (* = possible answer)")
+    cli.columnize(all_guesses_output[:n], displaywidth=80)
+    print("... top {}/{} shown \n".format(len(all_guesses_output[:n]), len(all_guesses_output)))
 
-    print("\nGuesses Ranked by Expected Information: \n")
-    print(next_guess.head())
+    print("Possible Answers Ranked by Information")
+    cli.columnize(all_answers_output[:n], displaywidth=80)
+    print("... top {}/{} shown \n".format(len(all_answers_output[:n]), len(all_answers_output)))
 
-    if len(lucky_guesses) == 0:
-        print("\nWe suggest picking: {} \n".format(list(guess_information.keys())[0]))
+    
+    for strategy in strategies_list:
         
-    elif len(lucky_guesses) == 1:
-        print("\nWe suggest picking: {} \n".format(lucky_guesses[0]))
+        next_guess = next_guess_options(guess_df, strategy)
         
-    else:
-        print("\nWe suggest picking one of these guesses, each are equally good, but you could get lucky! \n")
-        cli.columnize(list(lucky_guesses)[:30], displaywidth=80)
-        if len(lucky_guesses) > 30: print("...")
-            
-    print("\n")        
+        print("Using the strategy: {}, we suggest picking one of: ".format(strategy))
+        cli.columnize(next_guess, displaywidth=80)
+        print("\n")       
     
     
 def config(args):
